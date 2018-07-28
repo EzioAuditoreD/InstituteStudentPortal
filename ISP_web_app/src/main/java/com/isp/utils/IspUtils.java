ee87c72fb3ac53505ecddf26da4ffda3776d9541
abhis <abhis@EzioAuditoreD>
2018-07-28 18:11:09 +0530

    service and encoder utils with errors

---
+++ ISP_web_app/src/main/java/com/isp/utils/IspUtils.java
@@ -0,0 +1,9 @@
+package com.isp.utils;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class IspUtils {
+	public static PasswordEncoder passwordEncoder() {
+	    return new BCryptPasswordEncoder();
+}
